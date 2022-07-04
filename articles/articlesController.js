e924020cc2bdea54e13d8720745324833ed8a130
Joao PC Windos <augusto_casotti@hotmail.com>
2022-07-04 08:36:53 -0300

    Erro no forech

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -31,14 +31,13 @@ router.post("/articles/save",(req,res)=>{
 	})
 })
 router.get("/admin/articles/edit/:id",(req,res)=>{
-	let id =req.params.id;
+	const id =req.params.id;
 	Article.findByPk(id).then(article=>{
 		if (id!= undefined) {
 			Category.findAll().then(categories=>{
-
 				res.render("admin/articles/edit",{categories:categories,article:article})
 			})
-	
+		
 		} else {
 			res.redirect("/")
 		}
